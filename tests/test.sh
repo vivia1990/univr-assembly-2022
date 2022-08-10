dcea48e8f22621e486005baa224188330c49d1ba
vivia1990 <donvivia@gmail.com>
2022-08-10 18:16:52 +0200

    refactor(test): no default run

--- tests/test.sh
+++ tests/test.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-unit_test() {    
+unit_test() {
     local flags=(-I inc/ -m32 -g -no-pie)
-    local files=(temp.c src/utility.c src/telemetry.c ../src/telemetry.s)
+    local files=(temp.c src/utility.c src/telemetry.c ../src/telemetry.s ../src/utility.s)
     local main="#include \"test.h\" \n #include <string.h> \n \n #include <stdlib.h> \n #include <stdio.h> \n int main() {     :call_func;     return 0; } void custom_assert_fail(const char *__assertion, const char *counter) {     const char *msg = \"Fallito: \";     const size_t length = strlen(__assertion);     const char *expr = strcpy((char *)malloc(length), __assertion);     char *string = malloc(length + strlen(msg));     puts(strcat(strcat(string, msg), expr)); }" 
     
     name=$(echo "$1" | tr '[:upper:]' '[:lower:]')
     function="test_$name(0)"
     echo -e "${main/\:call_func/$function}" > temp.c
 
-    gcc "${flags[@]}" "${files[@]}" -o bin/test
-    ./bin/test
+    gcc "${flags[@]}" "${files[@]}" -o bin/test    
     rm -f temp.c
 }
 
